3651f1e938e1368cb8e83602dcce015074ad1742
CharlesLee-23 <Coleman.hallom@Spray.com>
2021-08-13 10:29:31 -0500

    other changes

--- src/PyTry.py
+++ src/PyTry.py
@@ -5,24 +5,27 @@ def main():
         print("Mortgage- " + "${:,.2f}".format(totalPrice))
         print("Number Years of for Mortgage?")
         years = int(input())
+        print("\n")
         downPayment = totalPrice / 10
-        print(" your down payment would be" + str(downPayment))
+        print("down payment would be " + "${:,.2f}".format(downPayment))
         netPrice = totalPrice - downPayment
-        print("With down payment applied, your total would be"  + "${:,.2f}".format(totalPrice - downPayment))
+        print("\n")
+        print("With down payment applied, your total would be "  + "${:,.2f}".format(totalPrice - downPayment))
 
         while(years >30 ):
             print("Enter year less than 30")
             years = int(input())
-        print(str(years) + "-year" )
+        print("You chose a " + str(years) + "-year mortgage. " )
         mortgageMonths = years * 12
+        print("\n")
         monthlyPrice = float(round(netPrice/mortgageMonths,2))
 
         print("The monthly rent would be $" + str(monthlyPrice))
-
         print("What is your current monthly rent?")
         rentPay = int(input())
+        print("\n")
         percentage = round((monthlyPrice/rentPay)*100,2)
-        print("You would pay " + str(percentage) + "% of the rent that you pay now")
+        print("You would pay " + str(percentage) + "% of the rent that you pay now\n")
         print("If you paid your current rent, you would own it in " + str(round((totalPrice / rentPay)/12,0)) + " years")
         return rentPay
     
